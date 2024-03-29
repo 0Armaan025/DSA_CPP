f70d940fdf601d1a5d96579b546f9e5c3f4dbf22
Armaan <104704093+0Armaan025@users.noreply.github.com>
2024-03-29 17:29:55 +0530

    Update chocolate_solution.cpp

--- chocolate_solution.cpp
+++ chocolate_solution.cpp
@@ -2,36 +2,18 @@
 using namespace std;
 
 int main() {    
-
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
-
     int rupees = 15;
     int wrappers = 0;
-    int chocolates = 0;
-    bool inital = true;
-    while(rupees>=0 && inital) {
-        
-        if(wrappers == 0) {
-            chocolates = chocolates + rupees/1;
-            rupees = 0;
-            wrappers = wrappers + chocolates;
-        }
-        else if(wrappers %3 == 0){
-            
-                chocolates = chocolates + wrappers/3;
-                wrappers = wrappers + chocolates;
-                
-        
-        }
-        else {
-            inital=false;
-        }
+    int chocolates = rupees; // Initially, we buy chocolates with rupees
+    bool initial = true;
+    
+    while (initial || wrappers >= 3) {
+        initial = false;
+        chocolates += wrappers / 3; // Exchange wrappers for chocolates
+        wrappers = wrappers / 3 + wrappers % 3; // Remaining wrappers after exchange
     }
 
-    cout<<chocolates;
+    cout << chocolates;
 
     return 0;
 }
